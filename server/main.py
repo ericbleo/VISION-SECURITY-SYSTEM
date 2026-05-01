1c4c6d4ebd8b3038df468b281e58f3b8a105cae3
Eric Bwire <biyhem@gmail.com>
2026-05-01 14:53:38 +0300

    added waitKey(1) to render frames and respond to the window

--- server/main.py
+++ server/main.py
@@ -16,24 +16,26 @@ class SecuritySystem:
     def run(self):
         while True:
             ret, frame = self.video.read()
+            if not ret:
+                break
+            
             frame = cv2.flip(frame, 1)
             results = self.model(frame, stream=True)
 
             for r in results:
-                boxes = r.boxes
-                for box in boxes:
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    w = x2 - x1
-                    h = y2 - y1
+                for box in r.boxes:
+                    x1, y1, x2, y2 = [int(v) for v in box.xyxy[0]]
+                    w, h = x2 - x1, y2 - y1
                     conf = math.ceil((box.conf[0] * 100)) / 100
-                    cls = int(box.cls[0])
-                    name = self.class_names[cls]
+                    name = self.class_names[int(box.cls[0])]
 
                     if name == 'person':
                         cvzone.cornerRect(frame, (x1, y1, w, h), 10, 3)
                         cvzone.putTextRect(frame, f'{name} {conf}', (x1, y1 - 10), 1, 1, (0, 0, 0))
 
-                cv2.imshow("VISION SECURITY SYSTEM", frame)
+            cv2.imshow("VISION SECURITY SYSTEM", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
 
 if __name__ == "__main__":
